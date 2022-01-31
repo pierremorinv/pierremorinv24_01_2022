a5fc043d6f394bfafed373e26bc41412016c45e9
Pierre Morin <morinpierrevo@gmail.com>
2022-01-31 14:31:17 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const thing = require("./models/thing");
+mongoose
+  .connect(
+    "mongodb+srv://PierreVqzz:Tktpoulette33.@cluster0.sr5gy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  // accédez à notre API depuis n'importe quelle origine
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Ajouter les headers mentionnés aux requêtes envoyées vers notre API
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+
+  // envoyer des requêtes avec les méthodes mentionnées
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.post("/api/stuff", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !",
+  });
+});
+
+app.use("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
+});
+module.exports = app;
